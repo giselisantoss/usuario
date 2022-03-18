450427dcba3b71ef78b3979b8e1c5e990be7a739
A PONTEW <giselisantos2019@gmail.com>
2022-03-18 19:56:23 -0300

    codigo completo

---
+++ app.js
@@ -0,0 +1,10 @@
+function calculo(){
+    var usuario=prompt("Digite o seu usuario?")
+    var password=prompt("Digite sua senha")
+    
+    if (usuario == "usuario123" && password == "123456"){
+        alert("nome do usuario e senha estao corretos");
+    } else {
+        alert("nome do usuario e senha estao incorretos");
+ }
+    }
